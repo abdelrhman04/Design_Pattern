0486f45db562494a158b9bce43c735fc48240efc
abdelrhman04 <abdomajde04@gmail.com>
2023-04-19 12:45:45 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns;
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Singleton instance =Singleton.GetInstance();
+        instance.Addone();
+        Console.WriteLine(instance.count.ToString());
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+}
+
---
+++ ConsoleApp1/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns;
+public class Singleton
+{
+    public int count = 0;
+    private static Singleton instance = null;
+    private static object lockobj = new object();
+    private Singleton() { }
+    public static Singleton GetInstance(){
+        //Lazy Initialization
+        if (instance == null) { 
+            lock (lockobj)
+                {
+                   instance = new Singleton();
+                }
+        }
+        return instance;
+    }
+public void Addone() => count++;
+}
